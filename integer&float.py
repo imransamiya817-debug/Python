0acdfa9b56d3283f4b432d6f7f42d1bc90656b4a
SamiyaImran <imransamiya817@gmail.com>
2026-02-28 15:21:48 +0530

    Create integer&float.py

---
+++ integer&float.py
@@ -0,0 +1,62 @@
+# type function
+num = 3
+print(type(num))
+num = 3.12
+print(type(num))
+
+# Arithmetic operators
+# Addition: 2+2
+print(2+2)
+# Subtraction: 6-5
+print(6-5)
+# Multiplication: 3*2
+print(3*2)
+# Division: 7/2
+print(7/2)
+# Floor division: 7//2
+print(7//2)
+# Exponent: 3**2
+print(3**2)
+# Modulus: 3%2
+print(3%2)
+
+# Increment
+num = 1
+num = num + 1
+num += 1
+
+# Built-in functions
+# Abs
+print(abs(-3))
+
+# Round
+print(round(3.64))
+print(round(3.63,1))
+
+# Comparisons
+num_1 = 3
+num_2 = 7
+
+# Equal
+print(num_1 == num_2)
+
+# Not Equal
+print(num_1 != num_2)
+
+# Greater than
+print(num_1 > num_2)
+
+# Less than
+print(num_1 < num_2)
+
+# Greater or equal
+print(num_1 >= num_2)
+
+# Less or equal
+print(num_1 <= num_2)
+
+# Casting
+num = '56'
+print(type(num))
+num = int(num)
+print(type(num))
